3b76d88c012ee732ad85749397f2e453d759ed3a
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-11-10 23:05:37 +0100

    added logic to get user input, clean it and check if commands exists. If so, it calls back the correct function

--- repl.go
+++ repl.go
@@ -1,10 +1,59 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
 	cleaned := strings.Fields(strings.ToLower(text))
 	return cleaned
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := cleanInput(scanner.Text())[0]
+
+		cmd, exists := getCommands()[input]
+		if exists {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func getCommands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message and available commands.",
+			callback:    commandHelp,
+		},
+	}
+
+	return commands
+
 }
