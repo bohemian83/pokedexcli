4b17a0f35b8cbcd0b7948b7fc4cae35aa15bdb21
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-11-13 09:23:56 +0100

    added command to show next and previous map locations

---
+++ commandMap.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandMap(cfg *config) error {
+	response, err := cfg.pokeapiClient.GetLocationAreas(cfg.nextURL)
+	if err != nil {
+		return err
+	}
+
+	for _, result := range response.Results {
+		fmt.Println(result.Name)
+	}
+
+	cfg.nextURL = response.Next
+	cfg.previousURL = response.Previous
+
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.previousURL == nil {
+		fmt.Println("You are on the first page.")
+		return nil
+	}
+	response, err := cfg.pokeapiClient.GetLocationAreas(cfg.previousURL)
+	if err != nil {
+		return err
+	}
+
+	for _, result := range response.Results {
+		fmt.Println(result.Name)
+	}
+
+	cfg.nextURL = response.Next
+	cfg.previousURL = response.Previous
+
+	return nil
+}
