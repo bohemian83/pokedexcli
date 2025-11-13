5bdbb5d86d3c9a5861e0dbb729d47bcd67e989f7
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-11-13 09:23:07 +0100

    added map and mapb commands

--- repl.go
+++ repl.go
@@ -5,21 +5,25 @@ import (
 	"fmt"
 	"os"
 	"strings"
-)
 
-func cleanInput(text string) []string {
-	cleaned := strings.Fields(strings.ToLower(text))
-	return cleaned
-}
+	"github.com/bohemian83/pokedexcli/internal/pokeapi"
+)
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func startRepl() {
+type config struct {
+	nextURL       *string
+	previousURL   *string
+	pokeapiClient pokeapi.Client
+}
+
+func startRepl(cfg *config) {
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -28,7 +32,7 @@ func startRepl() {
 
 		cmd, exists := getCommands()[input]
 		if exists {
-			err := cmd.callback()
+			err := cmd.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -52,8 +56,22 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message and available commands.",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Shows the next 20 location areas.",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Shows the previous 20 location areas.",
+			callback:    commandMapb,
+		},
 	}
 
 	return commands
+}
 
+func cleanInput(text string) []string {
+	cleaned := strings.Fields(strings.ToLower(text))
+	return cleaned
 }
