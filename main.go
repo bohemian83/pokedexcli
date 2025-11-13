3936d341276c9f18725d7cbfe2f609502ed6a34a
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-11-13 18:47:50 +0100

    added todos

--- main.go
+++ main.go
@@ -5,6 +5,17 @@ import (
 	"time"
 )
 
+//TODO: Update the CLI to support the "up" arrow to cycle through previous commands
+//TODO: Simulate battles between pokemon
+//TODO: Add more unit tests
+//TODO: Refactor your code to organize it better and make it more testable
+//TODO: Keep pokemon in a "party" and allow them to level up
+//TODO: Allow for pokemon that are caught to evolve after a set amount of time
+//TODO: Persist a user's Pokedex to disk so they can save progress between sessions
+//TODO: Use the PokeAPI to make exploration more interesting. For example, rather than typing the names of areas, maybe you are given choices of areas and just type "left" or "right"
+//TODO: Random encounters with wild pokemon
+//TODO: Adding support for different types of balls (Pokeballs, Great Balls, Ultra Balls, etc), which have different chances of catching pokemon
+
 func main() {
 	pokeClient := pokeapi.NewClient(60*time.Second, 5*time.Minute)
 	cfg := &config{pokeapiClient: pokeClient}
