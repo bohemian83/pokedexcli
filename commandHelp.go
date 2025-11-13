984fd46bdf0df4f6d17e9d008abd23d1a08bfc8c
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2025-11-13 09:24:08 +0100

    passed config to commandHelp

--- commandHelp.go
+++ commandHelp.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
 	for _, cmd := range getCommands() {
 		fmt.Printf("%s, %s\n", cmd.name, cmd.description)
